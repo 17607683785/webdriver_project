bd614d9002cf29a4aee21413b1feb7c88818aef7
sky_Libing <107823857+17607683785@users.noreply.github.com>
2024-08-01 12:02:18 +0800

    app.py

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
 from selenium import webdriver
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium.webdriver.chrome.options import Options
 
 # 配置 WebDriver
+options = Options()
+options.add_argument('--headless')
+options.add_argument('--no-sandbox')
+options.add_argument('--disable-dev-shm-usage')
+
 driver = webdriver.Remote(
-    command_executor='http://selenium-hub:4444/wd/hub',
-    desired_capabilities=DesiredCapabilities.CHROME)
+    command_executor='http://selenium-hub:4444/wd/hub',  # 修改为你的WebDriver地址
+    options=options
+)
 
 # 打开一个网页
 driver.get("http://www.example.com")
@@ -14,3 +20,4 @@ input("Press Enter to close the browser...")
 
 # 关闭浏览器
 driver.quit()
+
