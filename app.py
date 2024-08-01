42731157d571bd0e6b923bc44f577bde327b47a6
sky_Libing <107823857+17607683785@users.noreply.github.com>
2024-08-01 17:45:11 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+import os
 
 app = Flask(__name__)
 
@@ -11,16 +12,18 @@ def index():
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
 
-    driver = webdriver.Remote(
-        command_executor='http://selenium-hub:4444/wd/hub',  # Selenium Server 地址
-        options=options,
-        desired_capabilities=DesiredCapabilities.CHROME
-    )
-
-    driver.get("http://www.example.com")
-    title = driver.title
-    driver.quit()
-    return f"Page title is {title}"
+    try:
+        driver = webdriver.Remote(
+            command_executor=f'http://{os.environ["SELENIUM_HUB_HOST"]}:{os.environ["SELENIUM_HUB_PORT"]}/wd/hub',
+            options=options,
+            desired_capabilities=DesiredCapabilities.CHROME
+        )
+        driver.get("http://www.example.com")
+        title = driver.title
+        driver.quit()
+        return f"Page title is {title}"
+    except Exception as e:
+        return str(e), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=10000)  # Flask 应用监听的端口
+    app.run(host='0.0.0.0', port=10000)
